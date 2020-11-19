38b4b41986a5b7b5d5975f30c6e770d5179c60a9
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-19 23:43:34 +0530

    [Ibraheem Khaleel] Added :  code for creating class for custom errors

--- user_registration/input_error.py
+++ user_registration/input_error.py
@@ -1,3 +1,10 @@
 class InputError(Exception):
+    """
+    Creating class for custom errors inherited from Exception class
+    """
     def __init__(self, message):
+        """
+        :parameter message: it is inherited from Exception class
+        :param message:
+        """
         super().__init__(message)
