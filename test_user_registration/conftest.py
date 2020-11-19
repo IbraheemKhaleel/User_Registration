68bc1c94736a13ab0ac4de304380ebc265e5b452
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-19 23:39:02 +0530

    [Ibraheem Khaleel] Added :  code for creating fixture methods which can be used in every test methods

---
+++ test_user_registration/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from user_registration.user_registration import UserRegistration
+
+
+@pytest.fixture
+def instance_of_main_class():
+    user_registration = UserRegistration()
+    return user_registration
