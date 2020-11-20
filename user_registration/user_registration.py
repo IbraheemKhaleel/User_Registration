e07a72dd87af96a948c3b52b14c9346230488256
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-20 08:30:06 +0530

    [Ibraheem Khaleel] Added :  code for craeting methods which validates the  email entry

--- user_registration/user_registration.py
+++ user_registration/user_registration.py
@@ -9,6 +9,7 @@ class UserRegistration:
     """
     NAME_PATTERN = r'^[A-Z][A-Za-z]{2,}$'
     PHONE_NUMBER_PATTERN = r'[9][1][ ][6-9]{1}[0-9]{9}$'
+    EMAIL_ID_PATTERN = r'^[A-Za-z0-9]+([-.+_]{1}[0-9A-Za-z]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,4}([.,]{1}[a-z]{2,3}){0,1}$'
 
     def validate_name(self, name):
         """
@@ -33,3 +34,17 @@ class UserRegistration:
             raise InputError(" Enter the 10 digit phone number with country code followed by space ")
         else:
             return True
+
+    def validate_email_id(self, email_id):
+        """
+        :param email_id:
+        :return: if email id  matches with pattern, return True
+        """
+        pattern = re.compile(self.EMAIL_ID_PATTERN)
+        match = pattern.search(email_id)
+        if not match:
+            return False
+        else:
+            return True
+
+
