e5045b60abdd70104b1ecf1927450df91ef6e221
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-20 07:53:30 +0530

    [Ibraheem Khaleel] Added :  code for testing methods which validates the  phone number entry

--- test_user_registration/test_user_registration.py
+++ test_user_registration/test_user_registration.py
@@ -21,3 +21,13 @@ def test_validate_last_name(instance_of_main_class):
 def test_invalidate_last_name(instance_of_main_class):
     with pytest.raises(InputError):
         instance_of_main_class.validate_name("b239 ")
+
+
+def test_validate_phone_number(instance_of_main_class):
+    result = instance_of_main_class.validate_phone_number("91 9745945143")
+    assert result.__eq__(True)
+
+
+def test_invalidate_phone_number_should_raise_exception(instance_of_main_class):
+    with pytest.raises(InputError):
+        instance_of_main_class.validate_phone_number("85875")
