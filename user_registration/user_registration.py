c0a57422bc1584289403b32722c9a6343efc809b
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-20 08:51:25 +0530

    [Ibraheem Khaleel] Added :  code for creating methods which validates the  password entry

--- user_registration/user_registration.py
+++ user_registration/user_registration.py
@@ -10,15 +10,26 @@ class UserRegistration:
     NAME_PATTERN = r'^[A-Z][A-Za-z]{2,}$'
     PHONE_NUMBER_PATTERN = r'[9][1][ ][6-9]{1}[0-9]{9}$'
     EMAIL_ID_PATTERN = r'^[A-Za-z0-9]+([-.+_]{1}[0-9A-Za-z]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,4}([.,]{1}[a-z]{2,3}){0,1}$'
+    PASSWORD_PATTERN = r'^(?=.*[A-Z])' + '(?=.*[a-z])' + '(?=.*[!@_#$%^&*])' + '(?=.*[0-9]).{8,}$'
+
+    def pattern_matching(self, input, pattern_input):
+        """
+        Creating a method for pattern matching
+        :param input:
+        :param pattern_input:
+        :return: matched input
+        """
+        pattern = re.compile(pattern_input)
+        match = pattern.search(input)
+        return match
 
     def validate_name(self, name):
         """
         :param name:
         :return: if  name matches with pattern, return True
         """
-        pattern = re.compile(self.NAME_PATTERN)
-        match = pattern.search(name)
-        if not match:
+        matched_input = self.pattern_matching(name, self.NAME_PATTERN)
+        if not matched_input:
             raise InputError(" Enter the first name with minimum three characters ")
         else:
             return True
@@ -28,9 +39,8 @@ class UserRegistration:
         :param phone_number:
         :return: if  phone number matches with pattern, return True
         """
-        pattern = re.compile(self.PHONE_NUMBER_PATTERN)
-        match = pattern.search(phone_number)
-        if not match:
+        matched_input = self.pattern_matching(phone_number, self.PHONE_NUMBER_PATTERN)
+        if not matched_input:
             raise InputError(" Enter the 10 digit phone number with country code followed by space ")
         else:
             return True
@@ -40,11 +50,19 @@ class UserRegistration:
         :param email_id:
         :return: if email id  matches with pattern, return True
         """
-        pattern = re.compile(self.EMAIL_ID_PATTERN)
-        match = pattern.search(email_id)
-        if not match:
+        matched_input = self.pattern_matching(email_id, self.EMAIL_ID_PATTERN)
+        if not matched_input:
             return False
         else:
             return True
 
-
+    def validate_password(self, password):
+        """
+        :param password:
+        :return: if password  matches with pattern, return True
+        """
+        matched_input = self.pattern_matching(password, self.PASSWORD_PATTERN)
+        if not matched_input:
+            raise InputError('Enter a valid password with one capital letter, special character and a number')
+        else:
+            return True
