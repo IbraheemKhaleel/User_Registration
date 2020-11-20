0a4cbe13442b6274c5794867420ff22396d7af92
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-20 07:42:10 +0530

    [Ibraheem Khaleel] Added :  code for testing methods which validates the  last name entry

--- test_user_registration/test_user_registration.py
+++ test_user_registration/test_user_registration.py
@@ -4,11 +4,20 @@ from user_registration.input_error import InputError
 
 
 def test_validate_first_name(instance_of_main_class):
-    result = instance_of_main_class.validate_first_name("Michael")
+    result = instance_of_main_class.validate_name("Michael")
     assert result.__eq__(True)
 
 
 def test_invalidate_first_name(instance_of_main_class):
     with pytest.raises(InputError):
-        instance_of_main_class.validate_first_name(" m9 ")
+        instance_of_main_class.validate_name(" m9 ")
 
+
+def test_validate_last_name(instance_of_main_class):
+    result = instance_of_main_class.validate_name("John")
+    assert result.__eq__(True)
+
+
+def test_invalidate_last_name(instance_of_main_class):
+    with pytest.raises(InputError):
+        instance_of_main_class.validate_name("b239 ")
