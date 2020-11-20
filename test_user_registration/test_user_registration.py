6c8c0f82562c88bf52b84e9d401484c6d6d7fba4
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-20 08:29:28 +0530

    [Ibraheem Khaleel] Added :  code for testing methods in a parameterized argument which validates the  email entry

--- test_user_registration/test_user_registration.py
+++ test_user_registration/test_user_registration.py
@@ -31,3 +31,32 @@ def test_validate_phone_number(instance_of_main_class):
 def test_invalidate_phone_number_should_raise_exception(instance_of_main_class):
     with pytest.raises(InputError):
         instance_of_main_class.validate_phone_number("85875")
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("abc@yahoo.com", True),
+    ("abc-100@yahoo.com", True),
+    ("abc.100@yahoo.com", True),
+    ("abc111@abc.com", True),
+    ("abc-100@abc.net", True),
+    ("abc.100@abc.com.au", True),
+    ("abc@1.com", True),
+    ("abc@gmail.com.com", True),
+    ("abc+100@gmail.com", True),
+    ("abc", False),
+    ("abc@.com.my", False),
+    ("abc123@.com", False),
+    ("abc123@.com.com", False),
+    ("abc123@gmail.a", False),
+    (".abc@abc.com", False),
+    ("abc()*@gmail.com", False),
+    ("abc@%*.com", False),
+    ("abc..2002@gmail.com", False),
+    ("abc.@gmail.com", False),
+    ("abc@abc@gmail.com", False),
+    ("abc@gmail.com.1a", False),
+    ("abc@gmail.com.aa.au", False)
+])
+def test_validate_email_id(input, expected, instance_of_main_class):
+    result = instance_of_main_class.validate_email_id(input)
+    assert result == expected
